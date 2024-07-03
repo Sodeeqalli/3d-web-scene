6932408ca301ef92d5c330315f7dbf686b24913b
Alli Sodeeq <sodeeqalli@gmail.com>
2024-07-03 21:32:14 +0100

    Initial Commit

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import {arrow}  from '../assets/icons';
+
+const InfoBox = ({text, link, btnText})=>(
+    <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>  {text} </p>
+      <Link to={link} className='neo-brutalism-white neo-btn'>
+       { btnText}
+       <img src={arrow} className='w-4 h-4 object-contain'/>
+      </Link>
+       </div>
+  
+  )
+const renderContent = {
+    1:(
+        <h1 className='sm:text-xl sm:leading-snug text-center
+         neo-brutalism-blue py-4 px-8 text-white mx-5'>
+            Hi, I am <span className='font-semibold'>Alli</span>👨🏾‍💻 <br />
+            A Software Developer from Nigeria
+         </h1>
+    ),
+    2:(
+        <InfoBox
+        text="Worked on many mini projects and picked up some skills along the way"
+        link="/about"
+        btnText="Learn More"
+        />
+    ),
+    3:(
+        <InfoBox
+        text="Want to explore some of my projects?"
+        link="/projects"
+        btnText="Visit my Portfolio"
+        />
+    ),
+    4:(
+        <InfoBox
+        text="Need a dev to get some work done?"
+        link="/contact"
+        btnText="Let's Talk"
+        />
+    ),
+
+}
+
+
+
+const HomeInfo = ({currentStage}) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
